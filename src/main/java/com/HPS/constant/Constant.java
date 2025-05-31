2f9e02e622fec29bcf734697e94cbc72fc92c6e2
Anike-09 <aniketharugade009@gmail.com>
2025-05-31 15:10:19 +0530

    testing automatic build trigger

--- src/main/java/com/HPS/constant/Constant.java
+++ src/main/java/com/HPS/constant/Constant.java
@@ -8,5 +8,9 @@ public class Constant {
 	public static final String DELETE_SUCCESS = "Delete Successfully";
 	public static final String SOME_IS_WRONG = "Something is wrong";
 	public static final int MAX_OPTIONAL_HOLIDAYS_PER_YEAR = 5;
+	
+	public static final int MAX_OPTIONAL_HOLIDAYS_PER = 5;
+	
+	public static final int MAX_OPTIONAL_HOLIDAYS_PERYEAR = 5;
 
 }
