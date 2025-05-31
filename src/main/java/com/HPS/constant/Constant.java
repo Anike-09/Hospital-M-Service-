bec1465f450bbaad46e672da4f007c27199ec3bc
Anike-09 <aniketharugade009@gmail.com>
2025-05-31 14:52:57 +0530

    test webhook trigger

---
+++ src/main/java/com/HPS/constant/Constant.java
@@ -0,0 +1,12 @@
+package com.HPS.constant;
+
+public class Constant {
+	
+	public static final Object OTP_SEND_SUCCESS = "OTP send successful...";
+	public static final String PASSWORD_PATTERN = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]).{8,}$";
+	public static final String SOMETHING_WENT_WRONG = "Something is wrong ";
+	public static final String DELETE_SUCCESS = "Delete Successfully";
+	public static final String SOME_IS_WRONG = "Something is wrong";
+	public static final int MAX_OPTIONAL_HOLIDAYS_PER_YEAR = 5;
+
+}
